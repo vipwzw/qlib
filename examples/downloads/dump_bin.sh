46dad4c788d24a544ae54f8a137fc346ddbecf98
vipwzw <vipwzw@vip.qq.com>
2025-04-11 23:55:43 +0800

    dump bin

--- examples/downloads/dump_bin.sh
+++ examples/downloads/dump_bin.sh
@@ -1,4 +1,4 @@
-python scripts/dump_bin.py dump_all \
+python ../../scripts/dump_bin.py dump_all \
   --csv_path data/binance/1m \
   --qlib_dir ~/.qlib/qlib_data/binance \
   --date_field_name datetime \
